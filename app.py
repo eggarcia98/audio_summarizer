0bbb4d81bdeb7ac36a777b85a60398927d1b9abc
Erick Garcia <eggarcia9814@gmail.com>
2024-05-28 07:37:22 +1000

    cleaning code app.py

--- app.py
+++ app.py
@@ -3,30 +3,27 @@ from services.audio_service import process_audio_file
 from utils.text_processing_utils import recognize_speech
 from lib.ntlp import segment_text
 import asyncio
+from datetime import datetime
+
 
 
 app = Flask(__name__)
 
 
 
-@app.route('/process', methods=['POST'])
+@app.route('/summarize_audio', methods=['POST'])
 def process_audio_file_endpoint():
-    print("HERE")
-    # Call audio processing function
     file = request.data
-    print("HERE2")
 
+    now = datetime.now()
 
-     # Define the path and name for the output file
-    output_file_path = 'output.mp3'
-    with open(output_file_path, 'wb') as output_file:
+    output_audio_path = f"{now} + .mp3"
+    with open(output_audio_path, 'wb') as output_file:
         output_file.write(file)
 
-    print("HERE3")
+    output_audio_path = process_audio_file(output_audio_path)
 
-    output_file_path = process_audio_file(output_file_path)
-
-    transcript = asyncio.run(recognize_speech(output_file_path))
+    transcript = asyncio.run(recognize_speech(output_audio_path))
     print("HERE 5")
     # time_stamps = segment_text(transcript)
 
--- services/audio_service.py
+++ services/audio_service.py
@@ -1,20 +1,9 @@
-# process_audio_file.py
-import librosa
-import numpy as np
 from pydub import AudioSegment
 
-# Extract audio track from video (if applicable)
-def process_audio_file(file_path):
-    output_file_path = f"{file_path}.wav"
+def process_audio_file(output_audio_path):
+    output_wav_audio_path = f"{output_audio_path}.wav"
 
-    audio_no_wav = AudioSegment.from_file(file_path)
-    audio_no_wav.export(output_file_path, format="wav")
+    audio_no_wav = AudioSegment.from_file(output_audio_path)
+    audio_no_wav.export(output_wav_audio_path, format="wav")
 
-    audio_wav, sr = librosa.load(output_file_path, sr=44100)
-    audio_spect = librosa.feature.melspectrogram(y=audio_wav, sr=sr)
-
-    # # Convert to log scale (dB). We'll use the peak power as reference.
-    # log_s = librosa.amplitude_to_db(S=audio_spect, ref=np.max )
-    # features = librosa.feature.mfcc(S=log_s, n_mfcc=13)
-
-    return output_file_path
+    return output_wav_audio_path
