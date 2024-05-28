57a6a5f747da2ca4ceace35adfdc8643d2a111eb
Erick Garcia <eggarcia9814@gmail.com>
2024-05-28 17:59:54 +1000

    modularizing recognize_speech function

--- services/speech_recognition/recognition.py
+++ services/speech_recognition/recognition.py
@@ -3,8 +3,9 @@ import asyncio
 import speech_recognition as sr
 from pydub import AudioSegment
 from services.speech_recognition.transcriptor import transcribe_audio
+from utils.audio_processor import audio_splitter, queue_audios_chunks
 
-async def recognize_speech_chunk(audio_chunk, start_time):
+async def recognize_speech_chunk(audio_chunk, start_time=0):
     """Recognize speech in a given audio chunk."""
     recognizer = sr.Recognizer()
     transcripts = []
@@ -23,17 +24,14 @@ async def recognize_speech_chunk(audio_chunk, start_time):
                 )
     return transcripts
 
-async def recognize_speech(audio_file_path, chunk_length_ms=30000):
+
+
+async def recognize_speech(audio_file_path):
     """Recognize speech in an audio file by splitting it into chunks."""
     audio = AudioSegment.from_wav(audio_file_path)
 
-    tasks = []
-    for start_ms in range(0, len(audio), chunk_length_ms):
-        end_ms = min(start_ms + chunk_length_ms, len(audio))
-        audio_chunk = audio[start_ms:end_ms]
-        start_time = start_ms / 1000
-        task = asyncio.create_task(recognize_speech_chunk(audio_chunk, start_time))
-        tasks.append(task)
+    audio_chunks = audio_splitter(audio)
+    audios_to_process = queue_audios_chunks(audio_chunks, recognize_speech_chunk)
 
-    transcripts = await asyncio.gather(*tasks)
+    transcripts = await asyncio.gather(*audios_to_process)
     return [transcript for sublist in transcripts for transcript in sublist]
--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,6 +1,9 @@
-"""Module to load local audios"""
-from pydub import AudioSegment
+"""Module to manage async functions"""
+import asyncio
 import os
+from pydub import AudioSegment
+from operator import itemgetter
+
 
 def process_audio_file(output_audio_path):
     """Function to convert format audio to wav"""
@@ -8,8 +11,32 @@ def process_audio_file(output_audio_path):
 
     audio_no_wav = AudioSegment.from_file(output_audio_path)
     audio_no_wav.export(output_wav_audio_path, format="wav", codec="pcm_s16le")
-    
+   
     return output_wav_audio_path
+
+def audio_splitter(audio, chunk_length_ms=30000):
+    """Function to split an audio into chunks with a specific length in ms"""
+    audio_chunks = []
+
+    for start_ms in range(0, len(audio), chunk_length_ms):
+        end_ms = min(start_ms + chunk_length_ms, len(audio))
+        audio_content = audio[start_ms:end_ms]
+        start_time = start_ms / 1000
+        chunk_data = dict({"start_time": start_time, "audio_content": audio_content})
+        audio_chunks.append(chunk_data)
+
+    return audio_chunks
+
+def queue_audios_chunks(audio_chunks, promise_recognize_speech):
+    """Function that create a list of audios chunks pending to process"""
+    audios_to_process = []
+
+    for audio_chunk in audio_chunks:
+        audio_content, start_time = itemgetter('audio_content', 'start_time')(audio_chunk)
+        task = asyncio.create_task(promise_recognize_speech(audio_content, start_time))
+        audios_to_process.append(task)
+
+    return audios_to_process
 
 
 def audio_remover(audio_path):
