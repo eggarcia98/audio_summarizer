c9349b504f23e98af8f7aec5baaa4a2f273ed572
Erick Garcia <eggarcia9814@gmail.com>
2024-06-12 16:59:12 +1000

    Speech Recognition library changed to whisper, funtions recognize_speech and merge_overlapping_transcript no more used

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 
 from utils.audio_processor import process_audio_file, audio_remover, get_audio_from_youtube
-from utils.transcript_processor import merge_overlapping_transcripts
-from services.speech_recognition.recognition import recognize_speech
+
+import whisper
+
+# Cargar el modelo Whisper
+model = whisper.load_model("base")
+
+# Transcribir el archivo de audio
+
 
 app = Flask(__name__)
 
@@ -44,13 +50,13 @@ def process_audio_file_endpoint():
             output_file.write(audio_bytes)
 
     wav_audio_path = process_audio_file(source_audio_path)
-    transcript = asyncio.run(recognize_speech(wav_audio_path))
+    # transcript = asyncio.run(recognize_speech(wav_audio_path))
+    result = model.transcribe(wav_audio_path)
     
     audio_remover(source_audio_path)
     audio_remover(wav_audio_path)
 
-    merged_transcript = merge_overlapping_transcripts(transcript)
-    return jsonify({'data': merged_transcript}), 200
+    return jsonify({'data': result}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=6030)
