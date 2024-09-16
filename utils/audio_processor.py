de5e16331871dbce970b01c9dcd0766400526a18
Erick Garcia <eggarcia9814@gmail.com>
2024-09-16 13:19:21 +1000

    removing unused functions on audio_processor file

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,12 +1,16 @@
 """Module to manage async functions"""
-import asyncio
-import os
-from operator import itemgetter
-from pydub import AudioSegment
-import  yt_dlp
 import hashlib
+import os
 
- # client to many multimedia portals
+import yt_dlp
+
+def generate_audio_hash_identificator(audio_payload):
+    """Create a new SHA-256 hash object"""
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(audio_payload)
+
+    return sha256_hash.hexdigest()
+
 
 def get_audio_from_youtube(yt_url):
     """function to get an audio from youtube"""
@@ -37,13 +41,6 @@ def get_audio_from_youtube(yt_url):
     
     return downloaded_audio_dict
 
-def generate_audio_hash_identificator(audio_payload):
-    """Create a new SHA-256 hash object"""
-    sha256_hash = hashlib.sha256()
-    sha256_hash.update(audio_payload)
-
-    return sha256_hash.hexdigest()
-
 def get_audio_from_audio_file(audio_file):
     """Function to get an audio from audio bytes"""
     audio_bytes = audio_file.read()
@@ -58,40 +55,6 @@ def get_audio_from_audio_file(audio_file):
         'filename': destine_audio_path,
         "id": generate_audio_hash_identificator(audio_bytes)
     })
-
-def process_audio_file(output_audio_path):
-    """Function to convert format audio to wav"""
-    output_wav_audio_path = f"{output_audio_path}.wav"
-
-    audio_no_wav = AudioSegment.from_file(output_audio_path)
-    audio_no_wav.export(output_wav_audio_path, format="wav", codec="pcm_s16le")
-   
-    return output_wav_audio_path
-
-def audio_splitter(audio, chunk_length_ms=30000):
-    """Function to split an audio into chunks with a specific length in ms"""
-    audio_chunks = []
-
-    for start_ms in range(0, len(audio), chunk_length_ms):
-        end_ms = min(start_ms + chunk_length_ms, len(audio))
-        audio_content = audio[start_ms:end_ms]
-        start_time = start_ms / 1000
-        chunk_data = dict({"start_time": start_time, "audio_content": audio_content})
-        audio_chunks.append(chunk_data)
-
-    return audio_chunks
-
-def queue_audios_chunks(audio_chunks, promise_recognize_speech):
-    """Function that create a list of audios chunks pending to process"""
-    audios_to_process = []
-
-    for audio_chunk in audio_chunks:
-        audio_content, start_time = itemgetter('audio_content', 'start_time')(audio_chunk)
-        task = asyncio.create_task(promise_recognize_speech(audio_content, start_time))
-        audios_to_process.append(task)
-
-    return audios_to_process
-
 
 def audio_remover(audio_path):
     """Function to remove files"""
