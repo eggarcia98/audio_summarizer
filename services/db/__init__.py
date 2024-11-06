9504eed32da1abe939050b55cf1aad315b9fccc4
Erick Garcia <eggarcia9814@gmail.com>
2024-11-06 10:22:51 +1000

    Defining SQL Alchemy Class

---
+++ services/db/__init__.py
@@ -0,0 +1,10 @@
+"""Module to init SQLAlchemy ORM"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def init_db(app):
+    """Initialize the database with the Flask app."""
+    db.init_app(app)
