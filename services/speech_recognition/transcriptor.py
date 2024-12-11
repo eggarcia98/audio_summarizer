aea321c4664c4017bf514a449e126f17c4d39729
Erick Garcia <eggarcia9814@gmail.com>
2024-12-11 16:37:49 +1000

    enabling torch to use gpu

--- services/speech_recognition/transcriptor.py
+++ services/speech_recognition/transcriptor.py
@@ -1,11 +1,15 @@
 """Module to manage speech audio recognition"""
 
 import os
+import torch
 
 import whisper
 
-WHISPER_MODEL = os.getenv("WHISPER_MODEL", "/var/lib/volumes/gcs/gcs-1/medium.en.pt")
-MODEL = whisper.load_model(WHISPER_MODEL)
+WHISPER_MODEL = os.getenv("WHISPER_MODEL", "medium")
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"  
+print("Device: ", DEVICE)
+
+MODEL = whisper.load_model(WHISPER_MODEL, device=DEVICE)
 
 
 def transcribe_audio(audio_path):
@@ -14,6 +18,7 @@ def transcribe_audio(audio_path):
     Returns the parsed transcript data.
     """
 
+    print("Device: ", DEVICE)
     result = MODEL.transcribe(audio_path)
 
     parsed_segments = [
