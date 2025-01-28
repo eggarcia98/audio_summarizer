4d517e591839b592f59dddd2c35601bc81804f03
Erick Garcia <eggarcia9814@gmail.com>
2025-01-28 15:04:39 +1000

    Refactor audio transcription to use external API instead of Whisper model

--- services/speech_recognition/transcriptor.py
+++ services/speech_recognition/transcriptor.py
@@ -2,29 +2,53 @@
 
 import os
 
-import whisper
+import requests
 
-WHISPER_MODEL = os.getenv("WHISPER_MODEL", "medium")
+WHISPER_API_URL = os.getenv("API_URL", "http://localhost:8081/get-transcript-audio")
 
 
 def transcribe_audio(audio_path):
     """
-    Transcribes the audio file using the Whisper model.
+    Transcribes the audio file using the external API service.
     Returns the parsed transcript data.
     """
+    try:
+        # Open the audio file in binary mode
+        with open(audio_path, "rb") as audio_file:
+            print(audio_file)
+            # Send a POST request with the audio file
+            response = requests.post(
+                WHISPER_API_URL,
+                files={"file": audio_file},  # Attach the file to the 'file' field
+                timeout=60 * 10,  # Set a timeout of 60 * 10 seconds
+            )
 
-    MODEL = whisper.load_model(WHISPER_MODEL)
-    result = MODEL.transcribe(audio_path)
+            # Check if the request was successful
+            if response.status_code != 200:
+                print(f"Error: Received status code {response.status_code}")
+                print("Response:", response.text)
+                return []
+            # Parse the JSON response
 
-    parsed_segments = [
-        {
-            "audio_end_time": segment["end"],
-            "audio_start_time": segment["start"],
-            "transcript": segment["text"].strip(),
-        }
-        for segment in result["segments"]
-    ]
+            data = response.json()
 
-    transcript = parsed_segments
+            transcription_raw_data = data.get("transcription", "")
 
-    return transcript
+            segments = transcription_raw_data.get("segments", [])
+
+            parsed_segments = [
+                {
+                    "audio_end_time": segment["end"],
+                    "audio_start_time": segment["start"],
+                    "transcript": segment["text"].strip(),
+                }
+                for segment in segments
+            ]
+
+            print("Transcription:", parsed_segments)
+
+            return parsed_segments
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
